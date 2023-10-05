7973dac99379bac3ad3b02500e64683869ab0ae2
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-10-05 20:13:52 +0530

    ignited app using parcel

--- namastereact/App.js
+++ namastereact/App.js
@@ -1,16 +1,5 @@
-{
-  /* <div id="parent">
-  <div id="child">
-  <h1>I am h1</h1>
-  <h2>I am h2</h2>
-  </div>
-   <div id="child2">
-  <h1>I am h1</h1>
-  <h2>I am h2</h2>
-  </div>
-</div>; */
-}
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1!"),
@@ -22,11 +11,6 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 console.log(parent);
-//Because of this complex structure JSX introduced
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
---
+++ namastereact/Readme.md
@@ -0,0 +1,22 @@
+# Namaste React
+
+npm doesn't have a full form, it manages packages, but it is not node package manager.
+
+# Parcel
+
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm- written by C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundle
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling- support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking-remove unused code
